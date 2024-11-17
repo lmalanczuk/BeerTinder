e363f2865d664a55cf14be9514022ca918032b89
Łukasz Małańczuk <l.malanczuk03@gmail.com>
2024-11-17 16:26:18 +0100

    init

---
+++ src/main/java/com/bt/beertinder/BeertinderApplication.java
@@ -0,0 +1,13 @@
+package com.bt.beertinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeertinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeertinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bt/beertinder/BeertinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bt.beertinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeertinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
